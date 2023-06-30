4c761654c457f2c2c0cebdfac09068694da3626c
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-06-30 15:05:50 -0400

    Filter & Reset

--- tracalorie-app/js/app.js
+++ tracalorie-app/js/app.js
@@ -51,6 +51,13 @@ class CalorieTracker {
         }
     }
 
+    reset() {
+        this._totalCalories = 0;
+        this._meals = [];
+        this._workouts = [];
+        this._render();
+    }
+
     // Private methods
 
     _displayCaloriesTotal() {
@@ -184,6 +191,10 @@ class App {
         document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
         document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
 
+        document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'))
+        document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'))
+
+        document.getElementById('reset').addEventListener('click', this._reset.bind(this));
     }
 
     _newItem(type, e) {
@@ -226,6 +237,26 @@ class App {
                 e.target.closest('.card').remove();
             }
         }
+    }
+
+    _filterItems(type, e) {
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll(`#${type}-items .card`).forEach(item => {
+            const name = item.firstElementChild.firstElementChild.textContent;
+            if (name.toLowerCase().indexOf(text) !== -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        })
+    }
+
+    _reset() {
+        this._tracker.reset();
+        document.getElementById('meal-items').innerHTML = '';
+        document.getElementById('workout-items').innerHTML = '';
+        document.getElementById('filter-meals').value = '';
+        document.getElementById('filter-workouts').value = '';
     }
 }
 
